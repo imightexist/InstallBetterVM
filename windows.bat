6909dd3b35630f5a033af93e68ec89c61154b1e4
imightexist <94249457+imightexist@users.noreply.github.com>
2021-12-28 11:23:11 -0600

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -3,6 +3,7 @@ echo BetterVM version of Windows mkcvm
 echo Note: this is untested
 timeout /t 5 > nul
 title Checking Windows version
+for /f "tokens=4-5 delims=. " %%i in ('ver') do set VERSION=%%i.%%j
 if %version% == "10.0" goto mkbettervm
 if %version% == "6.3" goto mkbettervm
 if %version% == "6.2" goto mkbettervm
